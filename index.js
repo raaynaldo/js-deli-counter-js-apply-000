e8fb2b6cb6b3ba510f17e9001d23be26ca82f9b8
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-08-16 06:38:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ function takeANumber(currentLine, newPerson){
   currentLine.push(newPerson);
   return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`;
 }
+
+function nowServing()
